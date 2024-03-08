953184fc48c107b2d69361ff17ea5511275d225f
Matthew Leingang <leingang@nyu.edu>
2024-03-07 19:41:03 -0500

    Convert to python package

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # Stamp Class Dates on Files
 
+I print a class roster every day and use it to take attendance and record small todos.
 
+It seemed like a nice idea to stamp the class date on the PDF. And then it
+snowballed into this package.
+
+## Installation
+
+    pipx ensurepath
+    pipx install --editable .
+
+## Usage
+
+    stamp-date --calendar <iCal source> input.pdf
+
+## Printing
+
+I do this by lpr so that I can get them stapled at the same time.
+
+But because of spaces in file names this can be tricky.
+
+See https://stackoverflow.com/questions/1806868/linux-replacing-spaces-in-the-file-names
---
+++ stamp_date/cli.py
@@ -0,0 +1,76 @@
+import click
+import datetime
+import os.path
+import tempfile
+
+from icalevents.icalevents import events
+
+from stamp_date.dateutils import end_of_year
+from stamp_date.stamping import make_stamp, stamp
+from stamp_date.events import Event
+
+
+@click.command(help="Stamp class dates over a PDF file")
+@click.argument("calendar")
+@click.argument("input")
+@click.option(
+    "--stamp-date-format",
+    help="strftime format for date stamp",
+    default="%A %B %-d, %Y %-H:%M %p",
+)
+@click.option("--start",
+    type=click.DateTime(),
+    default=datetime.date.today().isoformat(),
+    help="Find class meetings after this date (default: today)")
+@click.option("--end",
+    type=click.DateTime(),
+    default=end_of_year(datetime.date.today()).isoformat(),
+    help="Find events before this date (default: end of current year)")
+def stamp_date(calendar: str, input: str, stamp_date_format: str,start, end):
+    print(f"{start=}, {end=}")
+    class_meetings = [
+        event
+        for event in [Event(e) for e in events(calendar, start=start, end=end)]
+        if event.is_lesson
+    ]
+    for meeting in class_meetings:
+        date = meeting.start.astimezone()
+        stamp_text = date.strftime(stamp_date_format)
+        print(f"{stamp_text=}")
+        output_pdf_path = "Attendance Form " + date.strftime("%Y-%m-%d") + ".pdf"
+        # Call the stamp_text function
+        stamp_path = tempfile.TemporaryFile()
+        make_stamp(stamp_text, stamp_path)
+        stamp(input, stamp_path, output_pdf_path)
+        print(f"Text stamped successfully. Output PDF saved at: {output_pdf_path}")
+
+
+
+if __name__ == "__main__":
+
+    input_pdf_path = os.path.join(
+        "..", "..", "Rosters", "2024-02-06", "NYU_RS_PHOTO.pdf"
+    )
+    # stamp_path = os.path.join(".", "stamp.pdf")
+    stamp_date_format = "%A %B %-d, %Y %-H:%M %p"
+
+    calendar = "https://brightspace.nyu.edu/d2l/le/calendar/feed/user/feed.ics?feedOU=342055&token=a3do9fw67ngkljbc18c33"
+    today = datetime.date.today()
+    eoy = end_of_year(today)
+    print(f"{today=}, {eoy=}")
+    class_meetings = [
+        event
+        for event in [Event(e) for e in events(calendar, start=today, end=eoy)]
+        if event.is_lesson
+    ]
+    # class_meetings = events(calendar,start=today,end=eoy)
+    for meeting in class_meetings:
+        date = meeting.start.astimezone()
+        stamp_text = date.strftime(stamp_date_format)
+        print(f"{stamp_text=}")
+        output_pdf_path = "Attendance Form " + date.strftime("%Y-%m-%d") + ".pdf"
+        # Call the stamp_text function
+        stamp_path = tempfile.TemporaryFile()
+        make_stamp(stamp_text, stamp_path)
+        stamp(input_pdf_path, stamp_path, output_pdf_path)
+        print(f"Text stamped successfully. Output PDF saved at: {output_pdf_path}")
